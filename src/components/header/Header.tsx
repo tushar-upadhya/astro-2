909a77e976e2205d70426932256a4ec789daa164
Tushar Upadhyay <tusharupadhyay690@gmail.com>
2024-11-11 20:28:18 +0530

    .

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,22 @@
+const Header = () => {
+  return (
+    <div className="">
+      <div className="container">
+        <div className="flex justify-between">
+          <div>
+            <img src="../../../public/assets/images/logo.svg" alt="logo" />
+          </div>
+          <div>
+            <div className="size-10 relative">
+              <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+                <div className="w-5 h-0.5 bg-zinc-300"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
